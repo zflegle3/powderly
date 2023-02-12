ae20d9abd3bb2941fbfad48e0a855a613ef1b6c5
Zachary Flegle <zflegle3@gmail.com>
2023-02-11 21:55:09 -0500

    Pulled map bounds and used to filter out locations within viewport

--- src/components/LocationMarker.js
+++ src/components/LocationMarker.js
@@ -1,11 +1,14 @@
 import {Icon} from '@iconify/react'
 import locationIcon from "@iconify/icons-mdi/fire-alert"
+import { GoogleMap, useLoadScript, MarkerF, getBounds, Marker } from "@react-google-maps/api"
 
 const LocationMarker = ({lat, lng, onClick}) => {
     return (
         <div className="location-marker" onClick={onClick}>
             <Icon icon={locationIcon} className='location-icon' />
         </div>
+
+        <Marker></Marker>
     )
 };
 
