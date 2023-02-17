dabfc3279e0c9417bcf3ce414616155afb3b3813
Zachary Flegle <zflegle3@gmail.com>
2023-02-17 01:52:48 -0500

    Refactored ConditionsDisplay component to include conditions in a seperate display component only visable when expanded, added animation to icon

---
+++ src/components/ConditionsDetail.js
@@ -0,0 +1,99 @@
+import Timeline from "./calendar/Timeline";
+import { FaSort, FaPlus, FaStar, FaRegStar, FaLocationArrow, FaCrosshairs } from 'react-icons/fa';
+import { useState } from "react";
+
+
+function ConditionsDetail({resortData}) {
+    const [displayStatus, setDisplayStatus]  = useState(false);
+
+    let cmToIn = 2.54;
+    //format data for display
+    let baseDepth = (resortData.conditions.current.baseDepth/cmToIn).toFixed(1);
+    let topDepth = (resortData.conditions.current.topDepth/cmToIn).toFixed(1);
+    let snow24 = (resortData.conditions.current.snow24/cmToIn).toFixed(1);
+    let snow72 = (resortData.conditions.current.snow24/cmToIn).toFixed(1);
+    let desc = resortData.conditions.current.description;
+    if (desc.length < 1) {
+        desc = "Snow Condition Unknown"
+    };
+    let fresh = resortData.conditions.current.freshSnow;
+    if (fresh !== "-") {
+        fresh = fresh/cmToIn.toFixed(1);
+    } else {
+        fresh = (0).toFixed(1);
+    }
+
+    const expandCollapse = () => {
+        if (displayStatus) {
+            console.log("close")
+            setDisplayStatus(false)
+        } else {
+            console.log("open")
+            setDisplayStatus(true);
+        }
+    }
+
+
+    return (
+        <div className="resort-conditions-detail">
+
+            <div className="snow-condition">
+                <p>Snow Condition:</p>
+                <p>{desc}</p>
+            </div>
+
+            <div className="resort-conditions-current">
+
+                <div className="twenty-four-hr-snow">
+                    <p>{fresh}"</p>
+                    <p>Fresh Snow</p>
+                </div>
+
+                <div className="twenty-four-hr-snow">
+                    <p>{snow24}"</p>
+                    <p>24 hrs</p>
+                </div>
+
+                <div className="seventy-two-hr-snow">
+                    <p>{snow72}"</p>
+                    <p>72 hrs</p>
+                </div>
+
+                <div className="conditions-detail">
+                    <p>{baseDepth}"</p>
+                    <p>Base Depth</p>
+                </div>
+
+                <div className="conditions-detail">
+                    <p>{topDepth}"</p>
+                    <p>Peak Depth</p>
+                </div>
+
+                {/* <div className="conditions-detail">
+                    <p>{baseDepth}" - {topDepth}"</p>
+                    <p>{desc}</p>
+                </div> */}
+
+                <div className="lifts-detail">
+                    <p>{resortData.lifts.open}/{resortData.lifts.total}</p>
+                    <p>Lifts Open</p>
+                </div>
+
+            </div>
+
+
+
+            <div className="resort-conditions-calendar">
+                <Timeline dataIn={resortData.conditions.history}/>
+            </div>
+
+            <div className="resort-conditions-forecast">
+                Add Forecast Data Here
+            </div>
+
+
+        </div>
+    );
+  }
+  
+  export default ConditionsDetail;
--- src/components/ConditionsDisplay.js
+++ src/components/ConditionsDisplay.js
@@ -1,6 +1,8 @@
 import Timeline from "./calendar/Timeline";
-import { FaSort, FaPlus } from 'react-icons/fa';
+import ConditionsDetail from "./ConditionsDetail";
+import { FaSort, FaPlus, FaStar, FaRegStar, FaLocationArrow, FaCrosshairs } from 'react-icons/fa';
 import { useState } from "react";
+import gsap from "gsap";
 
 
 function ConditionsDisplay({resortData}) {
@@ -25,102 +27,115 @@ function ConditionsDisplay({resortData}) {
 
     const expandCollapse = () => {
         if (displayStatus) {
-            console.log("close")
-            setDisplayStatus(false)
+            console.log("close");
+            minusToPlus();
+            setDisplayStatus(false);
         } else {
-            console.log("open")
+            console.log("open");
+            plusToMinus();
             setDisplayStatus(true);
         }
     }
+
+    const plusToMinus = () => {
+        gsap.to(`.bar-1-${resortData.refId}`, {
+            duration: 0.5,
+            rotation: 360,
+            delay: 0.2,
+        });
+        gsap.to(`.bar-2-${resortData.refId}`, {
+            duration: 0.5,
+            rotation: 360,
+            delay: 0.2,
+        });
+
+    }
+
+    const minusToPlus = () => {
+        //Rotate
+        gsap.to(`.bar-1-${resortData.refId}`, {
+            duration: 0.5,
+            rotation: 0,
+        });
+        gsap.to(`.bar-2-${resortData.refId}`, {
+            duration: 0.5,
+            rotation: 90,
+        });
+    }
+
+
+    let ratingVal = "na"
+    if (Number(resortData.conditions.forecast[0].rating) > 7.5) {
+        ratingVal = "best";
+    } else if (Number(resortData.conditions.forecast[0].rating) > 5) {
+        ratingVal = "good";
+    } else if (Number(resortData.conditions.forecast[0].rating) > 2.5) {
+        ratingVal = "ok";
+    } else if (Number(resortData.conditions.forecast[0].rating) > 0) {
+        ratingVal = "bad";
+    } 
 
 
     if (displayStatus) {
         return (
             <div className="resort-conditions-display">
-                <div className="resort-conditions-data">
+                <div className="resort-conditions-header">
     
-                    <div className="expand-collapse-icon" onClick={expandCollapse}>
-                        <FaPlus icon="fa-thin"/>
+                    <div className="expand-collapse-btn" onClick={expandCollapse}>
+                        <div className={`bar-1-${resortData.refId}`}></div>
+                        <div className={`bar-2-${resortData.refId}`}></div>
+                        {/* <div className={`bar-3-${resortData.refId}`}></div> */}
                     </div>
     
-                    <p className="rating">{resortData.conditions.forecast[0].rating}</p>
+                    <p className={`rating-${ratingVal}`}>{resortData.conditions.forecast[0].rating}</p>
     
                     <p className="resort-name">{resortData.name}</p>
-    
-                    <div className="twenty-four-hr-snow">
-                        <p>{fresh}"</p>
-                        <p>Fresh Snow</p>
+
+                    <div className="header-icon" onClick={expandCollapse}>
+                        <FaCrosshairs/>
                     </div>
-    
-                    <div className="twenty-four-hr-snow">
-                        <p>{snow24}"</p>
-                        <p>24 hrs</p>
-                    </div>
-    
-                    <div className="seventy-two-hr-snow">
-                        <p>{snow72}"</p>
-                        <p>72 hrs</p>
-                    </div>
-    
-                    <div className="conditions-detail">
-                        <p>{baseDepth}" - {topDepth}"</p>
-                        <p>{desc}</p>
-                    </div>
-    
-                    <div className="lifts-detail">
-                        <p>{resortData.lifts.open}/{resortData.lifts.total}</p>
-                        <p>Lifts Open</p>
+
+                    <div className="header-icon" onClick={expandCollapse}>
+                        <FaRegStar/>
                     </div>
     
                 </div>
-                <div className="resort-conditions-calendar">
+
+                {/* <div className="resort-conditions-calendar">
                     <Timeline dataIn={resortData.conditions.history}/>
-                </div>
+                </div> */}
+
+                <ConditionsDetail resortData={resortData} />
+
             </div>
         );
 
     } else {
         return (
             <div className="resort-conditions-display">
-                <div className="resort-conditions-data">
+                <div className="resort-conditions-header">
     
-                    <div className="expand-collapse-icon" onClick={expandCollapse}>
-                        <FaPlus icon="fa-thin"/>
+                    <div className="expand-collapse-btn" onClick={expandCollapse}>
+                        <div className={`bar-1-${resortData.refId}`}></div>
+                        <div className={`bar-2-${resortData.refId}`}></div>
+                        {/* <div className={`bar-3-${resortData.refId}`}></div> */}
                     </div>
     
-                    <p className="rating">{resortData.conditions.forecast[0].rating}</p>
+                    <p className={`rating-${ratingVal}`}>{resortData.conditions.forecast[0].rating}</p>
     
                     <p className="resort-name">{resortData.name}</p>
-    
-                    <div className="twenty-four-hr-snow">
-                        <p>{fresh}"</p>
-                        <p>Fresh Snow</p>
+
+                    <div className="header-icon" onClick={expandCollapse}>
+                        <FaCrosshairs/>
                     </div>
-    
-                    <div className="twenty-four-hr-snow">
-                        <p>{snow24}"</p>
-                        <p>24 hrs</p>
-                    </div>
-    
-                    <div className="seventy-two-hr-snow">
-                        <p>{snow72}"</p>
-                        <p>72 hrs</p>
-                    </div>
-    
-                    <div className="conditions-detail">
-                        <p>{baseDepth}" - {topDepth}"</p>
-                        <p>{desc}</p>
-                    </div>
-    
-                    <div className="lifts-detail">
-                        <p>{resortData.lifts.open}/{resortData.lifts.total}</p>
-                        <p>Lifts Open</p>
+
+                    <div className="header-icon" onClick={expandCollapse}>
+                        <FaRegStar/>
                     </div>
     
                 </div>
-                <div className="resort-conditions-calendar">
-                    {/* <Timeline display={displayStatus} dataIn={resortData.conditions.history}/> */}
-                </div>
+
+
             </div>
         );
     }
