b821eb44158f9cd02bdf70ed73aefbeab3b6a99e
Zachary Flegle <zflegle3@gmail.com>
2023-02-18 00:16:44 -0500

    Added modal components and redux state to hold user account modal and handle open/close functionality

---
+++ src/components/modals/ModalContainer.js
@@ -0,0 +1,45 @@
+import { useSelector, useDispatch } from 'react-redux';
+import { closeModal } from '../../features/modals/modalSlice';
+import ReactDom from "react-dom";
+//Components
+// import NewLeagueModal from "./NewLeagueModal";
+// import EditSettingsModal from './EditSettingsModal';
+import UserAccountModal from './UserAccountModal';
+
+
+
+function ModalContainer() {
+    const dispatch = useDispatch();
+    const { status } = useSelector((state) => state.modal)
+
+    const close = () => {
+        dispatch(closeModal());
+    }
+
+    if (status === "user-account") {
+        return ReactDom.createPortal(
+            <>  
+                <div className="modal-overlay" onClick={close}></div>
+                <UserAccountModal/>
+            </>,
+            document.getElementById("modal-portal")
+        );
+
+    } else {
+        return null;
+    }
+    // else if (status === "edit-settings") {
+    //     return ReactDom.createPortal(
+    //         <>  
+    //             <div className="modal-overlay" onClick={close}></div>
+    //             <EditSettingsModal />
+    //         </>,
+    //         document.getElementById("modal-portal")
+    //     );
+
+    // } 
+
+
+}
+
+export default ModalContainer;
