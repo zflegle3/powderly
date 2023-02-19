e24a66844cf7968ec02163624eeffe3b71daf0b1
Zachary Flegle <zflegle3@gmail.com>
2023-02-19 01:42:19 -0500

    Refactored sign in form validation to use functions from validation.js

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,22 +1,18 @@
-import axios from "axios";
 import { useState, useEffect } from 'react';
 import { Link, useNavigate} from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-// import { toast } from "react-toastify";
 import {register, resetUser } from "../../features/auth/authSlice";
-// import { ReactComponent as CheckSvg } from '../../images/icons/check.svg';
 import { FaRegCheckCircle, FaCheck, FaRegTimesCircle } from 'react-icons/fa';
+import {checkNewUserName, checkNewEmail, checkNewPass } from "../../features/auth/validation";
+
 //Components
 import LoadingSpinner from "../LoadingSpinner";
 import PasswordInput from "./PasswordInput"
 
 function SignUp() {
-    const [passStatus, setPassStatus] = useState("");//true when user is valid
-    // const [newUserValid, setNewUserValid] = useState("");
-    // const [newEmailValid, setNewEmailValid] = useState("");
-    // const [newPassValid, setNewPassValid] = useState("");
     const navigate = useNavigate();
     const dispatch = useDispatch();
+    const [passStatus, setPassStatus] = useState("");//true when user is valid
     const {user, isLoading, isError, isSuccess, message } = useSelector((state) => state.auth);
 
 
@@ -39,10 +35,6 @@ function SignUp() {
                     };
                     //dispatches register function from authSlice to create new user
                     dispatch(register(userData)); 
-
-                } else {
-                    document.querySelector(".form-item-container.pass-in").classList.add("invalid");
-                    document.getElementById("pass-error").textContent = "Password does not meet criteria";
                 }
             }
         } 
@@ -66,133 +58,6 @@ function SignUp() {
         }
     }
 
-    //USERNAME VALIDATION
-    async function checkNewUserName(userNameIn) {
-        //LENGTH
-        if (userNameIn.length > 2 && userNameIn.length < 16) {
-            //FORMAT (Numbers and letters only 0-XX characters)
-            if (validateUserNameFormat(userNameIn)) { 
-                // AVAILABILITY IN DATABASE
-                if ( await checkUserDb(userNameIn)) {
-                //returns true if username already exists
-                    document.querySelector(".form-item-container.user-name-in").classList.add("invalid");
-                    document.getElementById("user-name-error").textContent = `Whoops! ${userNameIn} is already taken`;
-                    return false;
-                } else {
-                    document.getElementById("user-name-error").className = "";
-                    document.getElementById("user-name-error").textContent = "";
-                    return true;
-                };
-            } else {
-                document.querySelector(".form-item-container.user-name-in").classList.add("invalid");
-                document.getElementById("user-name-error").textContent = `Only letter and numbers, between 3 to 15 characters`;
-            }
-        } else {
-            if (userNameIn.length < 1) {
-                document.querySelector(".form-item-container.user-name-in").classList.add("invalid");
-                document.getElementById("user-name-error").textContent = "Cannot be empty";
-            } else {
-                document.querySelector(".form-item-container.user-name-in").classList.add("invalid");
-                document.getElementById("user-name-error").textContent = `Only letter and numbers, between 3 to 15 characters`;
-            }
-        };
-    }
-
-
-    //EMAIL VALIDATION
-    async function checkNewEmail(emailIn) {
-        //LENGTH
-        if (emailIn.length > 0) {
-            //FORMAT
-            if (validateEmailFormat(emailIn)) {
-                //AVAILABILITY IN DATABASE
-                if (await checkEmailDb(emailIn)) {
-                    //returns true if email already exists
-                    document.querySelector(".form-item-container.email-in").classList.add("invalid");
-                    document.getElementById("email-error").textContent = `Whoops! ${emailIn} is already taken `;
-                    return false;
-                } else {
-                    document.getElementById("email-error").className = "";
-                    document.getElementById("email-error").textContent = "";
-                    return true;
-                };
-            } else {
-                document.querySelector(".form-item-container.email-in").classList.add("invalid");
-                document.getElementById("email-error").textContent = "Oops, that looks like an invalid email";
-            }
-        } else {
-            document.querySelector(".form-item-container.email-in").classList.add("invalid");
-            document.getElementById("email-error").textContent = "Cannot be empty";
-        };
-    }
-
-    async function checkUserDb (usernameIn) {
-        const response = await axios.post("http://localhost:8080/user/read/username", {username: usernameIn});
-        if (response.data.username === usernameIn) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-    async function checkEmailDb (userEmail) {
-        const response = await axios.post("http://localhost:8080/user/read/email", {email: userEmail});
-        if (response.data.email === userEmail) {
-            return true;
-        } else {
-            return false;
-        }
-    }
-
-
-    const validateEmailFormat = (email) => {
-        return email.match(
-          /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        );
-    };
-
-    const validateUserNameFormat = (userName) => {
-        return userName.match(/^[a-zA-Z0-9]+$/);
-    };
-
-    const checkNewPass = (passwordIn) => {
-        let passTest = true;
-        let passErrorLen = document.getElementById("pass-error-signin-length");
-        let passErrorUp = document.getElementById("pass-error-signin-upper");
-        let passErrorNum = document.getElementById("pass-error-signin-number");
-        let passErrorSp = document.getElementById("pass-error-signin-special");
-
-        //8 characters
-        if(passwordIn.length < 8) {
-            passTest = false;
-            passErrorLen.classList = "";
-        } else {
-            passErrorLen.classList = "valid";
-        }
-        //uppercase
-        if(!passwordIn.match(/[A-Z]/g)) {
-            passTest = false;
-            passErrorUp.classList = "";
-        } else {
-            passErrorUp.classList = "valid";
-        }
-        //number
-        if(!passwordIn.match(/[0-9]/g)) {
-            passTest = false;
-            passErrorNum.classList = "";
-        } else {
-            passErrorNum.classList = "valid";
-        }
-        // special character
-        if(!passwordIn.match(/[!@#\$%\^&\*]/g)) {
-            passTest = false;
-            passErrorSp.classList = "";
-        } else {
-            passErrorSp.classList = "valid";
-        }
-        return passTest;
-    };
-
     const addFocus = (e) => {
         e.target.parentElement.parentElement.classList.add("focus");
     }
@@ -200,7 +65,6 @@ function SignUp() {
     const removeFocus = (e) => {
         e.target.parentElement.parentElement.classList.remove("focus");
     }
-
 
     useEffect(() => {
         if(isError) {
