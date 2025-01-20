3499f9c9173f60d6d0febdb1ba641b3fcc25f57f
Zachary Flegle <zflegle3@gmail.com>
2025-01-20 11:25:49 -0700

    Updates

--- src/custom-styles/style.js
+++
@@ -1,7 +0,0 @@
-export const addFocus = (e) => {
-    e.target.parentElement.parentElement.classList.add("focus");
-}
-
-export const removeFocus = (e) => {
-    e.target.parentElement.parentElement.classList.remove("focus");
-}
